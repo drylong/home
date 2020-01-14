bdddcf213682018e434f33ea3708ac8e02a98c4f
long <long@123>
2020-01-14 17:40:54 +0800

    恢复后

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import { HashRouter, Route ,Switch} from 'react-router-dom'
 
 import  Nav from "./pages/Nav/Nav.js";
-import  Login from "A/pages/Login/Login.js";
-// import  Login from "../src/pages/Login/Login.js";
+// import  Login from "A/pages/Login/Login.js";
+import  Login from "../src/pages/Login/Login.js";
 import  Reg from "../src/pages/Reg/Reg.js";
 import  Errer from "./pages/Errer";
 
